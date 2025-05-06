fa8b823c1a042178761ea03ef325a9d95211ae60
Violet Alice Dwyer <38626923+violet-alice@users.noreply.github.com>
2025-05-05 23:20:05 -0700

    copy of changed portfolio site

---
+++ gatsby-config.js
@@ -0,0 +1,61 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+module.exports = {
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // Of course you can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
+    siteTitleAlt: `Violet Alice Dwyer`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Violet Alice Dwyer`,
+        short_name: `Violet Alice`,
+        description: `Personal Site for Violet Alice Dwyer`,
+        start_url: `/`,
+        background_color: `#141821`,
+        theme_color: `#ff9900`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+}
